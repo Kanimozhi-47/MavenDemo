9dfc1173505a5ceb1584eb720bd14402856e5421
kanimozhi-47 <kanimozhiviswa9876@gmail.com>
2026-07-29 23:06:06 +0530

    fixed code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ package com.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
         System.out.println("Hello this is maven pipeline!");
     }
 }
